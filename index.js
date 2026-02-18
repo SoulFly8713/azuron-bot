3fd047e39c4453e71755498f09e29907ef6dcb20
SoulFly8713 <107763749+SoulFly8713@users.noreply.github.com>
2026-02-18 22:11:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const {
 } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot aktif 🚀");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server aktif.");
+});
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
