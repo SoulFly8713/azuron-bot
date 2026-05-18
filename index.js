8c779c4f2a4fb461303203bb06d5872965c48e24
SoulFly_871 <107763749+SoulFly8713@users.noreply.github.com>
2026-05-18 16:40:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -796,7 +796,7 @@ client.on('guildMemberAdd', async member => {
             const chatEmbed = new EmbedBuilder()
                 .setColor(0x2B2D31)
                 .setDescription(`Seni aramızda görmek harika! Burada keyifli sohbetlere katılabilir ve kalıcı dostluklar kurabilirsin.\n\n⋆˚｡ <#1470356749847363745> Kanalını okumayı unutma.\n⋆˚｡ <#1453838829025759364> Kanalından duyuruları takip edebilirsin.`)
-                .setImage('https://media1.tenor.com/m/NiHF9ex_AR0AAAAd/chainsaw-man-power-csm.gif');
+                .setImage('https://imgur.com/a/q0MME2w');
             chatChannel.send({ content: `Merhaba, ${member}`, embeds: [chatEmbed] }).catch(()=>{});
         }
     }
